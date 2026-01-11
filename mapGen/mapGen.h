ccf4f9c4af9bbf87c1805c4edd0c57920c12d6af
Cezary Pińkowski <01197909@pw.edu.pl>
2026-01-11 12:29:19 +0100

    changed structure

---
+++ mapGen/mapGen.h
@@ -0,0 +1,5 @@
+double **generate_map(int w,int h,double strenght);
+double pythagoras(int a,int b);
+void print_map(double **map,int w, int h);
+void free_map(double **map, int h);
+void map_to_file(double **map, int w, int h,char *source);
