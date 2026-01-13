1d4a654264829f3e330f908c0ff5e91c0b5f58d3
jacek267 <01206782@pw.edu.pl>
2026-01-13 15:40:19 +0100

    map

---
+++ map.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "psoStructs.h"
+#include "map.h"
+
+map* load_map(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Nie mozna otworzyc pliku mapy");
+        return NULL;
+    }
+
+    map *m = malloc(sizeof(map));
+    if (!m) { fclose(file); return NULL; }
+
+    if (fscanf(file, "%d %d", &m->w, &m->h) != 2) {
+        fclose(file);
+        free(m);
+        return NULL;
+    }
+
+    m->plot = malloc(m->h * sizeof(double*));
+    for (int i = 0; i < m->h; i++) {
+        m->plot[i] = malloc(m->w * sizeof(double));
+        for (int j = 0; j < m->w; j++) {
+            fscanf(file, "%lf", &m->plot[i][j]);
+        }
+    }
+
+    fclose(file);
+    return m;
+}
---
+++ map.h
@@ -0,0 +1,7 @@
+#ifndef MAP_H
+#define MAP_H
+#include "psoStructs.h" 
+
+map* load_map(const char *filename);
+
+#endif
