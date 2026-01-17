685711c1558c3444691348091fa0ac5e6c59df21
Cezary Pińkowski <01197909@pw.edu.pl>
2026-01-17 21:50:47 +0100

    added options for map generating with perlin algorithm

--- mapGen/new_map_gen.c
+++ mapGen/new_map_gen.c
@@ -4,8 +4,32 @@
 #include <stdio.h>
 #include "new_map_gen.h"
 
-int main(){ //wymyśleć jak to odpalić
-    create_perlin_file(4,5,10,100);
+int main(int argc,char *argv[]){ //wymyśleć jak to odpalić
+    int w=5;
+    int h=5;
+    int sampling=10;
+    double max_val=100; 
+    
+    create_perlin_file(w,h,sampling,max_val);
+    for(int i=2;i<argc;i++){
+        if(strcmp(argv[i],"-w")==0){
+            w=atoi(argv[i+1]);
+            i++;
+        }
+        else if(strcmp(argv[i],"-h")==0){
+            h=atoi(argv[i+1]);
+            i++;
+        }
+        else if(strcmp(argv[i],"-s")==0){
+            sampling=atoi(argv[i+1]);
+            i++;
+        }
+        else if(strcmp(argv[i],"-n")==0){
+            max_val=atof(argv[i+1]);
+            i++;
+        }
+    }
+    return 0;
 }
 
 void create_perlin_file(int w,int h, int sampling,double max_val){  //result of it will be grid w*sampling x h*sampling with values in [0,max_val] range 
